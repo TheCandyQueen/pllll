8b5bd65abcad6ca54ec30273d071af28950ee2d7
TheCandyQueen <73914684+TheCandyQueen@users.noreply.github.com>
2021-02-03 16:42:10 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+var inputs = [];
+function getParagraph()
+     {
+          for(var i = 1 ; i <=6 ; i++)
+          {
+             inputs.push(document.getElementById("para1_input_box_" + i).value);
+             inputs.join(". ");
+             document.getElementById("showParagraph").innerHTML = inputs.join(". ");
+          }     
+     }
+
